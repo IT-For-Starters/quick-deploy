186f0a29776a767b7f505790c51c65ff21144c74
Steve at ITFS <122524858+itfs-steve@users.noreply.github.com>
2025-01-16 13:47:02 +0000

    Update run.ps1

--- win/run.ps1
+++ win/run.ps1
@@ -1,19 +1,26 @@
 
 #### Changable Variables!
 $githubLocation = "https://api.github.com/repos/it-for-starters/quick-deploy/contents/win"
+$BearerToken = Get-Content -Path "C:\temp\key.key"
+$Headers = @{
+    Authorization = "Bearer $BearerToken"
+}
+
 
 $destLocation = "C:\temp\quick-deploy\scripts_to_run"
-
 
 function DownloadGithubFolder {
     param (
         [string]$url
     )
 
+    Write-Host "Processing URL $url"
+
     # Fetch the list of repository contents
     try {
-        $githubresponse = Invoke-RestMethod -Uri $url
-    } catch {
+        $githubresponse = Invoke-RestMethod -Uri $url -Headers $Headers
+    }
+    catch {
         Write-Error "Failed to fetch repository contents. Check the repository URL or network connection."
         return $false
     }
@@ -21,26 +28,35 @@ function DownloadGithubFolder {
 
     foreach ($item in $githubresponse) {
         if ($item.type -eq "dir") {
+            Write-Host "Is folder. Will recurse."
             DownloadGithubFolder -url $item.url
-        } else {
-            $partialPath = $item.path.Replace("/","\")
+        }
+        else {
+            Write-Host "Is file. Will download."
+            $partialPath = $item.path.Replace("/", "\")
             $newPath = "$destLocation\$partialPath"
             $newPathDir = Split-Path -Path $newPath -Parent
             
             if (!(Test-Path -Path $newPathDir -PathType Container)) {
+                Write-Host "Path doesn't exist, will create"
                 New-Item -ItemType Directory -Path $newPathDir
             }
-            try {
-                Invoke-WebRequest -Uri $item.download_url -OutFile $newPath
-                if ([System.IO.Path]::GetExtension($newPath) -eq ".ps1") {
-                    Write-Host "Running file $newPath"
-                    & $newPath
+            if ($item.name -ne "run.ps1") {
+                Write-Host "Downloading now"
+                try {
+                    Invoke-WebRequest -Uri $item.download_url -OutFile $newPath
+                    if ([System.IO.Path]::GetExtension($newPath) -eq ".ps1") {
+                        Write-Host "Running file $newPath"
+                        & $newPath
+                    }
+                    
                 }
-                
-            } catch {
-                Write-Error "Failed to download file $($newPath)"
-                return $false
+                catch {
+                    Write-Error "Failed to download file $($newPath)"
+                    return $false
+                }
             }
+            
 
 
             
