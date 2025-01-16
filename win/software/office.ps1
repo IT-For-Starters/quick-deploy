268afd8d78ec6a4c7f0c21d25670b6f199843c1d
Steve at ITFS <122524858+itfs-steve@users.noreply.github.com>
2025-01-16 11:45:45 +0000

    Update office.ps1

--- win/software/office.ps1
+++ win/software/office.ps1
@@ -17,7 +17,7 @@ Start-Transcript -Path "$scriptLocation\LOG_$timestamp.txt"
 #### Step 1 - Download Installer
 $installerDir = "$scriptLocation\files";
 $installerPath = "$installerDir\$installerName"
-$installerTempPath = "$installerDir\$installerName\temp"
+$installerTempPath = "$installerDir\temp"
 
 Write-Host "[DL] Checking if installer Directory Exists"
 if (!(Test-Path -Path "$installerDir" -PathType Container)) {
@@ -38,8 +38,17 @@ if (!(Test-Path -Path "$installerTempPath" -PathType Container)) {
 Write-Host "[DL] Checking if installer exists already"
 if (!(Test-Path -Path "$installerPath" -PathType Leaf)) {
     Write-Host "[DL] No - downloading..."
-    $webClient = New-Object System.Net.WebClient
-    $webClient.DownloadFile($installerUrl, $installerPath)
+    $installerUrl
+    $installerPath
+    try {
+        Start-BitsTransfer -Source $installerUrl -Destination $installerPath
+        #$webClient.DownloadFile($installerUrl, $installerPath)
+    } catch {
+        Write-Host "Error message: $($_.Exception.Message)"
+        Write-Host "Stack trace: $($_.Exception.StackTrace)"
+        Write-Host "Inner Exception: $($_.Exception.InnerException)"
+    }
+    
 } else {
     Write-Host "[DL] Yes - proceeding..."
 }
@@ -47,19 +56,11 @@ if (!(Test-Path -Path "$installerPath" -PathType Leaf)) {
 #### Step 1.1 - Download Config
 $configPath = "$installerDir\$configName"
 
-Write-Host "[DL] Checking if installer Directory Exists"
-if (!(Test-Path -Path "$installerDir" -PathType Container)) {
-    Write-Host "[DL] No - creating..."
-    New-Item -Path "$installerDir" -ItemType Directory | Out-Null
-} else {
-    Write-Host "[DL] Yes - proceeding..."
-}
-
 Write-Host "[CONF] Checking if config exists already"
 if (!(Test-Path -Path "$configPath" -PathType Leaf)) {
     Write-Host "[CONF] No - downloading..."
-    $webClient1 = New-Object System.Net.WebClient
-    $webClient1.DownloadFile($configUrl, $configPath)
+    Start-BitsTransfer -Source $configUrl -Destination $configPath
+    #$webClient.DownloadFile($configUrl, $configPath)
 } else {
     Write-Host "[CONF] Yes - proceeding..."
 }
@@ -70,7 +71,14 @@ if (!(Test-Path -Path "$configPath" -PathType Leaf)) {
 #### Step 2 - Run Installer
 Write-Host "[INS1] Running Installer to get ODT"
 $installerArguments = "/extract:`"$installerTempPath`" /log:`"$installerTempPath\OfficeInstall.log`" /quiet /norestart"
-$installProcess = Start-Process -FilePath "$installerPath" -ArgumentList $installerArguments -Wait -PassThru
+try {
+    $installProcess = Start-Process -FilePath "$installerPath" -ArgumentList $installerArguments -Wait -PassThru
+} catch {
+    Write-Host "Error message: $($_.Exception.Message)"
+        Write-Host "Stack trace: $($_.Exception.StackTrace)"
+        Write-Host "Inner Exception: $($_.Exception.InnerException)"
+}
+
 
 $exitcode = $installProcess.ExitCode
 Write-Host "[INS1] Software Installation Exit Code is $exitcode"
@@ -82,16 +90,19 @@ if ($installProcess.ExitCode -eq 0) {
 else {
     # Installation failed
     Write-Error "[INS1] Installation Failed"
-    Remove-Item -Path $installerDir -Recurse -Force -Confirm:$true
+    #Remove-Item -Path $installerDir -Recurse -Force -Confirm:$false
     Stop-Transcript
 
     exit 1
 }
 
+Set-Location "$installerTempPath"
+
+
 #### Step 3 - Run Download
 Write-Host "[INSDL] Running Installer to Download Software"
 $installerArguments = "/download `"$configPath`""
-$installProcess = Start-Process -FilePath "$installerTempPath\Setup.exe" -ArgumentList $installerArguments -Wait -PassThru
+$installProcess = Start-Process -FilePath "$installerTempPath\setup.exe" -ArgumentList $installerArguments -Wait -PassThru
 
 $exitcode = $installProcess.ExitCode
 Write-Host "[INSDL] Software Installation Exit Code is $exitcode"
@@ -102,15 +113,15 @@ if ($installProcess.ExitCode -eq 0) {
 else {
     # Installation failed
     Write-Error "[INSDL] Installation Failed"
-    Remove-Item -Path $installerDir -Recurse -Force -Confirm:$true
+    Remove-Item -Path $installerDir -Recurse -Force -Confirm:$false
     Stop-Transcript
     exit 1
 }
 
-#### Step 3 - Run Install
-Write-Host "[INSSW] Running Installer to Install Software"
+#### Step 4 - Run Install
+Write-Host "[INSSW] Running Installer to Download Software"
 $installerArguments = "/configure `"$configPath`""
-$installProcess = Start-Process -FilePath "$installerTempPath\Setup.exe" -ArgumentList $installerArguments -Wait -PassThru
+$installProcess = Start-Process -FilePath "$installerTempPath\setup.exe" -ArgumentList $installerArguments -Wait -PassThru
 
 $exitcode = $installProcess.ExitCode
 Write-Host "[INSSW] Software Installation Exit Code is $exitcode"
@@ -120,11 +131,11 @@ if ($installProcess.ExitCode -eq 0) {
 }
 else {
     # Installation failed
-    Write-Error "[INSDL] Installation Failed"
-    Remove-Item -Path $installerDir -Recurse -Force -Confirm:$true
+    Write-Error "[INSSW] Installation Failed"
+    Remove-Item -Path $installerDir -Recurse -Force -Confirm:$false
     Stop-Transcript
     exit 1
 }
-Remove-Item -Path $installerDir -Recurse -Force -Confirm:$true
+Remove-Item -Path $installerDir -Recurse -Force -Confirm:$false
 Stop-Transcript
 exit 0
